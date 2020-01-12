63d9f273ef7c0101f92c502c01c8b8f144414bcc
Nitisakkooch <58855588+Nitisakkooch@users.noreply.github.com>
2020-01-12 23:50:33 +0700

    modify TT

--- lab4/Moveable.java
+++
@@ -1,7 +0,0 @@
-package koochaiyaphum.nitisak.lab4;
-
-public interface Moveable {
-	void accelerate() ;
-	void brake();
-	void setSpeed();
-}
--- lab4/Refuelable.java
+++
@@ -1,5 +0,0 @@
-package koochaiyaphum.nitisak.lab4;
-
-public interface Refuelable {
-	public void refuel();
-}
